f0dfdf14c0c9844e7e98c4f019a74c7caf453036
hotfixlive <calculus_y@hotmail.com>
2022-09-28 20:11:14 -0400

    dayThree

--- weekOne/dayThree.js
+++ weekOne/dayThree.js
@@ -1,3 +1,5 @@
+
+/*
 function day(day33) {
     console.log("this is Day 3");
 }
@@ -35,6 +37,7 @@ printRange(3,10)
 
 // while loop
 
+/*
 function printRange1(start,end) {
     let i = start;
     while (i <= end) {
@@ -43,4 +46,65 @@ function printRange1(start,end) {
     }
 }
 
-printRange1(7,1000000)
+printRange1(7,20)
+
+let i = 20;
+while ( i === 20) {
+    console.log(i);
+    
+}
+
+
+let i = 20;
+while ( true ) {
+    console.log(i);
+    
+}
+
+let i = 20;
+while ( false ) {
+    console.log(i);
+    
+}
+*/
+/*
+function skipFive(num) {
+    for (let i = 1; 1 <= num; i++) {
+        if (i === 5) {
+            continue
+        }
+        console.log(i);
+    }
+}
+
+skipFive(20);
+*/
+
+function breakAtFive(num) {
+    for (let i = 1; i <= num; i++) {
+        if (num === 5) {
+            break;
+        }
+        console.log(i);
+    }
+}
+breakAtFive(20);
+
+
+
+
+// a number than is less than 2
+// a prime number is only divisible by itself
+function isPrime(num) {
+    if (num < 2) return false;
+    for (let n = 2; n < num ; n++) {
+        if (num % n === 0) {
+            return false;
+        }
+    }
+            return true;
+}
+
+console.log(isPrime(7))
+console.log(isPrime(11))
+
